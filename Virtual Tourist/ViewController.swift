177dfcde97935b26a8f64d5a360a4a376c5e6398
Brent Mifsud <brent.mifsud@outlook.com>
2019-07-09 19:28:06 -0400

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Brent Mifsud on 2019-07-09.
+//  Copyright © 2019 Brent Mifsud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
