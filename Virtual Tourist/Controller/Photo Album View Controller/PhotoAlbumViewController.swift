a8583dd90cc6e6bea6cd5b7bf615e31ce3446c7a
Brent Mifsud <18543934+BrentMifsud@users.noreply.github.com>
2019-09-13 19:20:33 -0400

    get album collection view working with 100 photos.

--- Virtual Tourist/Controller/Photo Album View Controller/PhotoAlbumViewController.swift
+++ Virtual Tourist/Controller/Photo Album View Controller/PhotoAlbumViewController.swift
@@ -111,4 +111,5 @@ class PhotoAlbumViewController: UIViewController {
 		destinationVC.fetchedResultsViewController = fetchedResultsController
 		destinationVC.fetchedResultsViewControllerDelegate = self
 	}
+
 }
